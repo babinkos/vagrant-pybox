23b5090e97313517a20937f5d00b18462eea7ee9
Konstantin Babin <37809416+babinkos@users.noreply.github.com>
2018-04-09 11:05:51 +0300

    added shell script for steps from readme

---
+++ vbox-prepare.cmd
@@ -0,0 +1,8 @@
+vagrant up
+vagrant package --output pybox.box
+vagrant box add pybox pybox.box
+vagrant destroy -f
+echo "You can use now: "
+echo " boxname = 'pybox'"
+echo "in your Vagrantfile"
+
