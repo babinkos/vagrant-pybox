746c536617797cea2e88b3af4b6cfe7e763fe798
Konstantin Babin <37809416+babinkos@users.noreply.github.com>
2018-05-16 17:09:35 +0300

    fixed again apt sources.list

--- pybox-install.sh
+++ pybox-install.sh
@@ -2,8 +2,11 @@
 sudo chown vagrant /home/vagrant/.ssh/id_rsa
 sudo chmod 400 /home/vagrant/.ssh/id_rsa
 #ssh-keyscan -H localhost >> /home/vagrant/.ssh/known_hosts
-sudo sed -i 's%deb http://us.archive.ubuntu.com%deb http://by.archive.ubuntu.com%' /etc/apt/sources.list
-sudo sed -i 's%deb http://archive.ubuntu.com%deb http://by.archive.ubuntu.com%' /etc/apt/sources.list
+sudo sed -i 's%deb http:\/\/us.archive.ubuntu.com%deb http:\/\/by.archive.ubuntu.com%' /etc/apt/sources.list
+sudo sed -i 's%deb http:\/\/archive.ubuntu.com%deb http:\/\/by.archive.ubuntu.com%' /etc/apt/sources.list
+sudo sed -i 's%deb-src http:\/\/us.archive.ubuntu.com%deb-src http:\/\/by.archive.ubuntu.com%' /etc/apt/sources.list
+sudo sed -i 's%deb-src http:\/\/archive.ubuntu.com%deb-src http:\/\/by.archive.ubuntu.com%' /etc/apt/sources.list
+
 #sudo sed -i 's%archive.ubuntu.com%mirror.yandex.ru%' /etc/apt/sources.list
 sudo apt-get update
 sudo apt-get upgrade -y
--- vbox-prepare.sh
+++ vbox-prepare.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 vagrant up
 vagrant package --output pybox.box
-vagrant box add pybox pybox.box
+vagrant box add pybox pybox.box --force
 vagrant destroy -f
 rm pybox.box
 echo "You can use now: "
 echo " boxname = 'pybox'"
 echo "in your Vagrantfile"
-
