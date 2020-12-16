f4ac7a5f3204ba6314137246b4098a11073e6caf
q0s <37809416+babinkos@users.noreply.github.com>
2020-12-16 16:24:09 +0300

    save package log to file; fix permissions needed in libvirt case

--- vbox-prepare.sh
+++ vbox-prepare.sh
@@ -1,10 +1,18 @@
-#!/bin/sh
+#!/bin/bash
+set -ex
+sudo hostname
 vagrant up
-vagrant package --output pybox.box
+[ "$( stat --format %a /var/lib/libvirt/images/vagrant-pybox_pybox.img)" == "644" ] || sudo chmod a+r /var/lib/libvirt/images/vagrant-pybox_pybox.img
+vagrant halt
+#dpkg-statoverride --update --add root root 0644 /boot/vmlinuz-`uname -r`
+KERNVER=$(uname -r)
+[ "$( stat --format %a /boot/vmlinuz-${KERNVER} )" == "644" ] || sudo chmod 644 /boot/vmlinuz-${KERNVER}
+#LIBGUESTFS_DEBUG=1 LIBGUESTFS_TRACE=1 
+vagrant package --output pybox.box 2>&1 | tee /tmp/pybox-package.log
 vagrant box add pybox pybox.box
 vagrant destroy -f
 rm pybox.box
-echo "You can use now: "
-echo " boxname = 'pybox'"
-echo "in your Vagrantfile"
-
+sudo rm -v /var/lib/libvirt/images/vagrant-pybox_pybox.img
+#echo "You can use now: "
+#echo " boxname = pybox"
+#echo "in your Vagrantfile"
